0985089ac988c8bee1f433a8642139ae4efe62f6
erick <erick1204200100@gmail.com>
2026-01-30 11:44:56 -0300

    README

--- test/now.py
+++ test/now.py
@@ -2,6 +2,6 @@ import pprint
 import requests
 URL = 'http://127.0.0.1:5000/actual_environment'
 
-resp = requests.get(URL, params = {"latitude": 23.4558, "longitude": 45.6396, "detail" : True})
+resp = requests.get(URL, params = {"latitude": 52.5200, "longitude": 13.4050, "detail" : True})
 print(resp.status_code)
 pprint.pprint(resp.json())
--- test/post.py
+++ test/post.py
@@ -2,6 +2,6 @@ import pprint
 import requests
 URL = 'http://127.0.0.1:5000/my_opinion'
 
-resp = requests.post(URL, params = {"latitude": 23.4558, "longitude": 45.6396}, json = {"text": "A test", "name" : "testname"})
+resp = requests.post(URL, params = {"latitude": 23.4558, "longitude": 45.6396}, json = {"text": "A humidade está horrível", "name" : "Camaleão"})
 print(resp.status_code)
 pprint.pprint(resp.json())
